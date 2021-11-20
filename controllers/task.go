fd2393b692440a0972c51d716cf5a45faee96770
ansh-dev <32096193+anshul-repos@users.noreply.github.com>
2021-11-20 12:13:49 +0530

    Update task.go

--- controllers/task.go
+++ controllers/task.go
@@ -1,1 +1,59 @@
 
+package task
+
+import (
+  "github.com/gin-gonic/gin"
+  "go-todo/db"
+  "net/http"
+  // "github.com/gin-gonic/gin/binding"
+  "go-todo/models"
+)
+
+func GetTasks(c *gin.Context) {
+
+  var tasks []models.Task
+  db := db.GetDB()
+  db.Find(&tasks)
+  c.JSON(200, tasks)
+}
+
+func CreateTask(c *gin.Context) {
+  var task models.Task
+  var db = db.GetDB()
+
+  if err := c.BindJSON(&task); err != nil {
+    c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+      "error": err.Error(),
+    })
+    return
+  }
+  db.Create(&task)
+  c.JSON(http.StatusOK, &task)
+}
+
+func UpdateTask(c *gin.Context) {
+  id := c.Param("id")
+  var task models.Task
+
+  db := db.GetDB()
+  if err := db.Where("id = ?", id).First(&task).Error; err != nil {
+    c.AbortWithStatus(http.StatusNotFound)
+    return
+  }
+  c.BindJSON(&task)
+  db.Save(&task)
+  c.JSON(http.StatusOK, &task)
+}
+
+func DeleteTask(c *gin.Context) {
+  id := c.Param("id")
+  var task models.Task
+  db := db.GetDB()
+
+  if err := db.Where("id = ?", id).First(&task).Error; err != nil {
+    c.AbortWithStatus(http.StatusNotFound)
+    return
+  }
+
+	db.Delete(&task)
+}
